7ed47280acbf70e4209abc9328d2f391c135c84e
bb <26394523+broeknbytes@users.noreply.github.com>
2026-02-26 09:48:41 -0700

    chore(install): remove hardcoded rapidhash binary symlink

--- install.sh
+++ install.sh
@@ -11,6 +11,3 @@ for script in "$SCRIPT_DIR/bin"/*; do
   ln -sf "$script" "$TARGET_DIR/$script_name"
   echo "Linked $script_name"
 done
-
-ln -sf "$HOME/Code/GitHub/rapidhash/cli/build/rapidhash" "$TARGET_DIR/rapidhash"
-echo "Linked rapidhash_cli"
